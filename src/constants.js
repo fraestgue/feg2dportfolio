5de264f954d08be470784a1ad6a01ea6199f63bc
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-04-26 20:25:48 +0200

    a falta de enlazar

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,16 @@
 export const scaleFactor = 4
+
+export const dialogueData = {
+    pc: `Este es mi ordenador. Aquí es donde trabajo en Javascript, HTML y CSS.
+      Tengo algunos proyectos hechos que podrás encontrar en mi perfil de 
+       <a href="https://github.com/fraestgue" target="_blank">Github</a>!`,
+    "cs-degree": `Este es mi <a href="https://www.credential.net/85cdd616-1282-4df5-98fc-62142c20c40f#gs.7z9ur9" target="_blank">diploma de Full Stack Developer de Ironhack</a>! Lo tengo en la pared porque estoy muy orgulloso de él. Fue un Bootcamp muy intenso, pero creo que me ha proporcionado muchos recursos para trabajar como developer.`,
+    "sofa-table": `Este es mi sofá. Aquí me relajo viendo algunos tutoriales cortos o series`,
+    tv: `Esta es mi TV, con la que puedo desconectar viendo series o algún deporte`,
+    bed: `Aquí es donde duermo, pero también es donde pienso en posibles proyectos para el futuro`,
+    resume: `Este es mi escritorio y aquí está mi CV. <a href="https://www.canva.com/design/DAGAVwXOgRA/VR50YS0aMD2V_E7tRrO77A/edit?category=tACZCrL49rk" target="_blank">¿Quieres verlo?</a>
+    Contáctame a través de estepaguerrafrancisco@gmail.com si tienes alguna oportunidad laboral!`,
+    projects: `Puedes encontrar información sobre mis proyectos en mi <a href="https://github.com/fraestgue" target="_blank">Github</a> o en mi <a href="https://www.linkedin.com/in/francisco-estepa-guerra/" target="_blank">Linkedin</a>`,
+    library: `Aquí tengo algunos de libros para leer y relajarme como "El señor de los Anillos" o "Harry Potter. ¿Me recomendarías alguno?`,
+    exit: `Si quieres salir de este portfolio simplemente cierra la pestaña.`,
+  };
